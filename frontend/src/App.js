49fea6c2fefb3f6b1b9870a9afa5cf325ebb7296
prapawit patthasirivichot <60070045@kmitl.ac.th>
2021-04-05 11:20:22 +0700

    add tailwind framework

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p className="text-2xl">Hello Agape</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
