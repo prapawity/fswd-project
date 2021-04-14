136ea168cb9a885a2eefdec4182efc7a4b4f4cef
prapawit patthasirivichot <60070045@kmitl.ac.th>
2021-04-14 23:22:06 +0700

    hotfix-navbar

--- frontend/src/Components/General/UserNavBar.js
+++ frontend/src/Components/General/UserNavBar.js
@@ -101,7 +101,7 @@ const UserNavBar = (props) => {
                                         <div className="w-full">
                                         <span class="relative inline-block">
                                             <ShoppingCartIcon className="h-7 w-7 text-white-500" />
-                                            <span class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">{cart.length}</span>
+                                            <span class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">{cart?.length ?? 0}</span>
                                         </span>
                                         </div>
                                     </li>
