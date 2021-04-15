8dbadcdcdd5a93a21303aa6079998b1e4408a056
Pannita <60070061@kmitl.ac.th>
2021-04-15 17:15:28 +0700

    add order schema
    Backend- add order schema

--- backend/src/models/product-promotion.js
+++ backend/src/models/product-promotion.js
@@ -1,30 +1,35 @@
-import mongoose from 'mongoose'
-import { composeWithMongooseDiscriminators } from 'graphql-compose-mongoose'
+import mongoose from "mongoose";
+import { composeWithMongooseDiscriminators } from "graphql-compose-mongoose";
 
-const { Schema } = mongoose
+const { Schema } = mongoose;
 
-const DKey = 'type'
+const DKey = "type";
 
 const DataType = {
-  PROMOTION: 'PROMOTION',
-  PRODUCT: 'PRODUCT'
-}
+  PROMOTION: "PROMOTION",
+  PRODUCT: "PRODUCT",
+};
 
 const Category = {
-  RUNNING: 'RUNNING',
-  CASUAL: 'CASUAL',
-  FOOTBALL: 'FOOTBALL',
-  BASKETBALL: 'BASKETBALL',
-  SANDALS: 'SANDALS',
-  OTHER: 'OTHER'
-}
+  RUNNING: "RUNNING",
+  CASUAL: "CASUAL",
+  FOOTBALL: "FOOTBALL",
+  BASKETBALL: "BASKETBALL",
+  SANDALS: "SANDALS",
+  OTHER: "OTHER",
+};
 
 const ProductSchema = new Schema({
   name: {
-    type: String, required: true, index: true, unique: true
+    type: String,
+    required: true,
+    index: true,
+    unique: true,
   },
   price: {
-    type: String, required: true, index: true
+    type: String,
+    required: true,
+    index: true,
   },
   type: {
     type: String,
@@ -34,61 +39,80 @@ const ProductSchema = new Schema({
   category: {
     type: String,
     enum: Object.keys(Category),
-    default: Category.OTHER
+    default: Category.OTHER,
   },
   thumpnail: {
-    type: String
+    type: String,
   },
   imageList: {
-    type: [String]
+    type: [String],
   },
   timestamp: { type: Date, default: Date.now, index: true },
-  size: [{
-    size_number: {
-      type: String,
-      index: true
+  size: [
+    {
+      size_number: {
+        type: String,
+        index: true,
+      },
+      stock: {
+        type: Number,
+        index: true,
+      },
     },
-    stock: {
-      type: Number,
-      index: true
-    }
-  }],
+  ],
   description: {
-    type: String
-  }
-})
+    type: String,
+  },
+});
 
 const PromotionSchema = new Schema({
   productID: {
-    type: String, required: true, index: true, ref: 'Product'
+    type: String,
+    required: true,
+    index: true,
+    ref: "Product",
   },
   discount: {
-    type: Number, required: true, index: true
+    type: Number,
+    required: true,
+    index: true,
   },
   limit: {
     type: Number,
     index: true,
-    required: true
+    required: true,
   },
   status: {
     type: Boolean,
-    default: false
-  }
-})
+    default: false,
+  },
+});
 
 const discriminatorOptions = {
   fields: {
-    remove: ['category', 'imageList', 'thumnail', 'size']
-  }
-}
+    remove: ["category", "imageList", "thumnail", "size"],
+  },
+};
 
-ProductSchema.set('discriminatorKey', DKey)
+ProductSchema.set("discriminatorKey", DKey);
 
-export const BaseModel = mongoose.model('Product', ProductSchema)
-export const PromotionModel = BaseModel.discriminator(DataType.PROMOTION, PromotionSchema)
-export const ProductModel = BaseModel.discriminator(DataType.PRODUCT, ProductSchema)
-export const BaseTC = composeWithMongooseDiscriminators(BaseModel)
-export const PromotionTC = BaseTC.discriminator(PromotionModel, { name: DataType.PROMOTION, ...discriminatorOptions })
-export const ProductTC = BaseTC.discriminator(BaseModel, { name: DataType.PRODUCT, ...[] })
+export const BaseModel = mongoose.model("Product", ProductSchema);
+export const PromotionModel = BaseModel.discriminator(
+  DataType.PROMOTION,
+  PromotionSchema
+);
+export const ProductModel = BaseModel.discriminator(
+  DataType.PRODUCT,
+  ProductSchema
+);
+export const BaseTC = composeWithMongooseDiscriminators(BaseModel);
+export const PromotionTC = BaseTC.discriminator(PromotionModel, {
+  name: DataType.PROMOTION,
+  ...discriminatorOptions,
+});
+export const ProductTC = BaseTC.discriminator(BaseModel, {
+  name: DataType.PRODUCT,
+  ...[],
+});
 
-export default ProductModel
+export default ProductModel;
