dce1192fee37565b6f660d36167a85a172d76b3b
prapawit patthasirivichot <60070045@kmitl.ac.th>
2021-04-17 14:03:32 +0700

    add pagination, handle add to cart when not login

---
+++ frontend/src/Components/General/Pagination.js
@@ -0,0 +1,40 @@
+import { Fragment, useEffect, useState } from "react"
+
+const Pagination = (props) => {
+    const [stateIndex, setIndex] = useState(props?.index ?? 0)
+    const numberOfPage = props?.pages ?? 5
+    var pages = []
+    const content = () => {
+        pages = []
+        for (var i = 0; i < numberOfPage; i++) {
+            pages.push("")
+        }
+    }
+
+    const updateIndex = (newIndex) => {
+        setIndex(newIndex)
+        props?.updateIndex(newIndex)
+    }
+    content()
+    return (
+        <Fragment>
+            <div className="py-2" style={{ width: 'fit-content', margin: 'auto' }}>
+                <nav className="block">
+                    <ul className="flex pl-0 rounded list-none flex-wrap">
+                        {pages.map((_, index) => {
+                            return <li>
+                                <button onClick={() => {
+                                    updateIndex(index)
+                                }} href="#pablo" className={`focus:outline-none first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative border border-solid border-indigo-500  ${stateIndex === index ? "text-white bg-indigo-500" : "text-indigo-500"}`}>
+                                    {index + 1}
+                                </button>
+                                </li>
+                        })}
+                    </ul>
+                </nav>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Pagination
