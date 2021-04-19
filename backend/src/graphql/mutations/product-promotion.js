5a2bb0c6a37be580c1f19dd71aee2188e989461c
prapawit patthasirivichot <60070045@kmitl.ac.th>
2021-04-19 18:42:28 +0700

    create new branch
    Update product-promotion.js
    
    update promotion query

--- backend/src/graphql/mutations/product-promotion.js
+++ backend/src/graphql/mutations/product-promotion.js
@@ -3,3 +3,5 @@ import { ProductTC, PromotionTC } from "../../models";
 export const createProduct = ProductTC.getResolver('createOne')
 
 export const createPromotion = PromotionTC.getResolver('createOne')
+export const updatePromotion = PromotionTC.getResolver('updateOne')
+export const deletePromotion = PromotionTC.getResolver('removeOne')
