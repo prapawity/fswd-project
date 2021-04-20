34116f2b2bf270c7bea6844db14725d6310dff89
prapawit patthasirivichot <60070045@kmitl.ac.th>
2021-04-20 21:33:50 +0700

    add calculate TotalPrice, remove log

--- backend/src/graphql/relations/product-promotion.js
+++ backend/src/graphql/relations/product-promotion.js
@@ -1,4 +1,21 @@
-import { BaseTC, ProductTC, PromotionTC } from "../../models";
+import { schemaComposer } from "graphql-compose";
+import { ProductModel, ProductTC, PromotionTC } from "../../models";
+
+const customPricePromotion = schemaComposer.createResolver({
+    name: "customPricePromotion",
+    type: 'String',
+    args: {
+        productID: 'String!',
+        discount: 'Float!'
+    },
+
+    resolve: ( async ({ source, args, context, info }) => {
+        const product = await ProductModel.findById(args.productID).exec()
+
+        if (!product) return null
+        return (parseFloat(product.price) - parseFloat(product.price) * (args.discount / 100)).toString()
+    })
+})
 
 PromotionTC.addRelation(
     'productDetail', {
@@ -9,3 +26,12 @@ PromotionTC.addRelation(
     projection: { productID: true }
 }
 )
+
+PromotionTC.addRelation('totalPrice', {
+    resolver: () => customPricePromotion,
+    prepareArgs: {
+        productID: (source) => source.productID,
+        discount: (source) => source.discount
+    },
+    projection: { productID: true, discount: true },
+})
