43b6d66f64f7e85bc81984ecec979f15a8c52366
prapawit patthasirivichot <60070045@kmitl.ac.th>
2021-04-26 19:00:18 +0700

    edit order mutation

--- backend/src/graphql/mutations/order.js
+++ backend/src/graphql/mutations/order.js
@@ -2,3 +2,4 @@ import { OrderTC } from '../../models'
 import { needPermission } from '../queries/middleware'
 
 export const createOrder = OrderTC.getResolver('createOne', [needPermission])
+export const removeOrder = OrderTC.getResolver('removeOne', [needPermission])
