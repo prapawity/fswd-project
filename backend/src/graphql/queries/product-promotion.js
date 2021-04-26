9ca3c96a0bfe1b0a3b2f7ebceeeb9bdf14ca99a6
prapawit patthasirivichot <60070045@kmitl.ac.th>
2021-04-26 21:07:10 +0700

    remove permission promotion

--- backend/src/graphql/queries/product-promotion.js
+++ backend/src/graphql/queries/product-promotion.js
@@ -5,5 +5,5 @@ export const products = ProductTC.getResolver('findMany')
 export const product = ProductTC.getResolver('findOne')
 export const productByID = ProductTC.getResolver('findById')
 
-export const promotions = PromotionTC.getResolver('findMany', [needPermission])
+export const promotions = PromotionTC.getResolver('findMany')
 export const promotionByID = PromotionTC.getResolver('findById', [needPermission])
